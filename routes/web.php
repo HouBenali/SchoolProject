d9317bcd0fea43a4b3063a932c00b645cde43070
HoussamBenali <houssambenali@gmail.com>
2022-04-11 00:39:17 +0200

    Another commit

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,9 @@ Route::get('/Bulbasaur', [App\Http\Controllers\HomeController::class, 'index'])-
 Route::get('/Squirtle', [App\Http\Controllers\HomeController::class, 'index'])->name('tutorial');
 Route::get('/Charmander', [App\Http\Controllers\HomeController::class, 'index'])->name('tutorial');
 Route::get('/adventure', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/online', [App\Http\Controllers\WebController::class, 'index']);
 Route::get('/wildMap/{area}', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/pokemons', [App\Http\Controllers\web\WebController::class, 'index'])->name('pokemons');
 Route::get('/profile', [App\Http\Controllers\web\WebController::class, 'index'])->name('profile');
 Route::get('/shop', [App\Http\Controllers\web\WebController::class, 'index'])->name('shop');
 Route::get('/myshops', [App\Http\Controllers\web\WebController::class, 'index'])->name('myshops');
@@ -36,8 +38,7 @@ Route::get('/decks', [App\Http\Controllers\web\WebController::class, 'index'])->
 Route::get('/changeAvatar', [App\Http\Controllers\web\WebController::class, 'index'])->name('avatar');
 Route::get('/changePass', [App\Http\Controllers\web\WebController::class, 'index'])->name('changePass');
 Route::get('/changeProfile', [App\Http\Controllers\web\WebController::class, 'index'])->name('changeProfile');
-Route::get('/online', [App\Http\Controllers\WebController::class, 'index']);
-Route::get('/pokemons', [App\Http\Controllers\web\WebController::class, 'index'])->name('pokemons');
+
 
 Auth::routes();
 
